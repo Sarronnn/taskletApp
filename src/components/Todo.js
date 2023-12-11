ffe1fe9b454f01d4282de7bb73fa490d108f1c4f
Sarronnn <stadesse@lion.lmu.edu>
2023-12-10 17:31:23 -0800

    Fixed update method, good to sumbit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,14 +3,14 @@ import TodoForm from "./TodoForm";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
 
-function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
+function Todo({ todos, completeTodo, removeTodo, updatedTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
   });
 
   const submitUpdate = (value) => {
-    updateTodo(edit.id, value);
+    updatedTodo(edit.id, value);
     setEdit({
       id: null,
       value: "",
